5fdc246c663d5bd5a51a506dacdc98e54aa995a5
hiki <hiki@1215677336@qq.com>
2024-02-07 00:52:01 -0800

    log模块使用spdlog

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello world" << std::endl;
-    return 0;
+#include <iostream>  
+#include <spdlog/spdlog.h>  
+  
+int main() {  
+    // 创建并初始化一个logger    
+  
+    // 输出不同等级的日志信息  
+    spdlog::info("Welcome to spdlog!");
+    spdlog::warn("This is a warning message.");
+    spdlog::error("This is an error message.");
+    spdlog::critical("This is a critical message.");  
+    // logger->warn("This is a warning message.");  
+    // logger->error("This is an error message.");  
+    // logger->critical("This is a critical message.");  
+  
+    return 0;  
 }
